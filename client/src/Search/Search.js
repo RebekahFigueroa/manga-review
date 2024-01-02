a8a810c173d345624c442bdbbcd3eb6727eb3885
rebekahfigueroa <figueroarebekah@gmail.com>
2024-01-02 15:34:07 -0600

    added review toggles

--- client/src/Search/Search.js
+++ client/src/Search/Search.js
@@ -1,5 +1,6 @@
 import { Stack, TextField, Typography } from "@mui/material";
 import React from "react";
+import SearchCard from "./SearchCard";
 
 const Search = () => {
   return (
@@ -22,6 +23,26 @@ const Search = () => {
         variant="outlined"
         sx={{ width: "25%" }}
       />
+
+      <Stack
+        direction="row"
+        sx={{
+          justifyContent: "center",
+          gap: 2,
+          width: "80%",
+          flexWrap: "wrap",
+          mt: 1,
+          mb: 3,
+        }}
+      >
+        <SearchCard />
+        <SearchCard />
+        <SearchCard />
+        <SearchCard />
+        <SearchCard />
+        <SearchCard />
+        <SearchCard />
+      </Stack>
     </Stack>
   );
 };
--- client/src/Search/SearchCard.js
+++ client/src/Search/SearchCard.js
@@ -1,18 +1,41 @@
-import { Chip } from "@mui/material";
+import { Button, Chip } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useState } from "react";
 
 const SearchCard = ({}) => {
+  const [openView, setOpenView] = useState(false);
+  const handleClickOpenView = () => {
+    setOpenView(true);
+  };
+  const handleCloseView = () => {
+    setOpenView(false);
+  };
+
+  const [openWrite, setOpenWrite] = useState(false);
+  const handleClickOpenWrite = () => {
+    setOpenWrite(true);
+  };
+  const handleCloseWrite = () => {
+    setOpenWrite(false);
+  };
+
   return (
     <Card
       sx={{
-        maxWidth: 345,
+        maxWidth: 500,
         height: "25rem",
+        width: "20rem",
         display: "flex",
         flexDirection: "column",
       }}
@@ -34,12 +57,63 @@ const SearchCard = ({}) => {
           </Typography>
         </Box>
 
+        <Button variant="outlined" size="small" onClick={handleClickOpenView}>
+          View Reviews
+        </Button>
+        <Dialog open={openView} onClose={handleCloseView}>
+          <DialogTitle>Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To subscribe to this website, please enter your email address
+              here. We will send updates occasionally.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+        </Dialog>
+
+        <Button variant="outlined" size="small" onClick={handleClickOpenWrite}>
+          Write Review
+        </Button>
+        <Dialog open={openWrite} onClose={handleCloseWrite}>
+          <DialogTitle>Write A Review</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Please provide a detailed description of your thoughts on this
+              game.
+            </DialogContentText>
+            <TextField
+              sx={{ width: "100%" }}
+              id="outlined-multiline-flexible"
+              label="Review"
+              multiline
+              rows={10}
+              value="I enjoyed this game but I wish it had more than 5 hours of content."
+              // onChange={(e) => setSuggestionDescription(e.target.value)}
+              placeholder="Review"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseWrite}>Cancel</Button>
+            <Button onClick={handleCloseWrite}>Submit</Button>
+          </DialogActions>
+        </Dialog>
+
         <Typography variant="body2" color="text.secondary">
-          Conosole: Playstation 2
+          Single Player
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          6.5/10
         </Typography>
       </CardContent>
       <CardActions sx={{ marginTop: "auto" }}>
-        <Chip color="primary" label="Single Player" />
         <Chip color="primary" label="Action" />
         <Chip color="primary" label="Adventure" />
       </CardActions>
